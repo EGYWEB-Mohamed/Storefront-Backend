6a77595ea924e6de765ff78c647db064099654f6
Mohamed Said <ceo@egyweb.info>
2022-04-04 07:56:44 +0200

    Update Validations.ts
    - Add Validation to product_id and user_id to check if it already Exist or not

--- src/utilities/Validations.ts
+++ src/utilities/Validations.ts
@@ -1,4 +1,6 @@
 import validator from 'validator'
+import client from '../database'
+
 import { LoginType, UserType, ProductType, OrderType } from './Types'
 
 export const LoginValidator = async (params: LoginType): Promise<boolean | string> => {
@@ -24,6 +26,19 @@ export const ProductValidator = async (params: ProductType): Promise<boolean | s
 export const OrderValidator = async (params: OrderType): Promise<boolean | string> => {
   if (!params.product_id || !params.quantity || !params.user_id) {
     return 'Parameter {product_id & quantity & user_id} is Required'
+  }
+  const conn = await client.connect()
+  const checkProduct = (
+    await conn.query('SELECT * FROM products WHERE id = $1;', [params.product_id])
+  ).rowCount
+  const checkUser = (await conn.query('SELECT * FROM users WHERE id = $1;', [params.user_id]))
+    .rowCount
+  conn.release()
+  if (checkProduct == 0) {
+    return 'Product With id ' + params.product_id + ' Not Exist'
+  }
+  if (checkUser == 0) {
+    return 'User With id ' + params.user_id + ' Not Exist'
   }
   return true
 }
